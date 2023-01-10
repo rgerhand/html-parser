56500a874ad5e1eecba4a0a1c92701dfb065cec7
resti11 <rafalgerhand@gmail.com>
2023-01-10 19:31:08 +0100

    Refactor SQLiteClass.py and add @property to class methods

--- SQLiteClass.py
+++ SQLiteClass.py
@@ -4,37 +4,51 @@ from DataBaseHandler import DatabaseMain
 
 class SqliteClass(DatabaseMain):
     """ SQLITE Class"""
-    def __init__(self, database_name, table_name):
+    def __init__(self, database_name=None):
         """Sending parameters to create a base and table"""
-        self.con = sqlite3.connect(database_name)
-        self.table_name = table_name
+        self._con = None
+        if database_name != None:
+            self.create_database(database_name)
 
     def login(self, **kwargs):
         """ Login to database"""
         pass
 
-    def create_database(self, database_name):
-        """Create new database"""
-        pass
+    @property
+    def con(self):
+        return self._con
 
-    def create_table(self):
+    @property
+    def cursor(self):
+        return self.con.cursor()
+
+    def create_database(self, database_name_set):
+        """Set database name"""
+        self._con = sqlite3.connect(database_name_set)
+
+#TODO: **kwargs
+    def create_table(self, table_name, **kwargs):
         """ Create table with two rows"""
-        cur = self.con.cursor()
-        cur.execute(f"CREATE TABLE {self.table_name}(country text, population text)")
+        self.cursor.execute(f"CREATE TABLE {table_name}(country text, population text)")
 
-    def insert_into_table(self):
+#TODO: **kwargs
+    def insert_into_table(self, table_name):
         """ Insert data into table"""
-        cur = self.con.cursor()
+        table_name = SqliteClass.create_table
         sql_formula = f'''
-                INSERT INTO {self.table_name} (country, population) VALUES (?,?)'''
+                INSERT INTO {table_name} (country, population) VALUES (?,?)'''
         test1 = ('test1', '12321')
-        cur.execute(sql_formula, test1)
+        self.cursor.execute(sql_formula, test1)
         self.con.commit()
 
+#TODO: switch database
+#TODO: fluent pattern
 
-sqlite1 = SqliteClass('test2.db', 'tabela1')
-sqlite1.login()
-sqlite1.create_table()
-sqlite1.insert_into_table()
+
+sqlite1 = SqliteClass()
+sqlite1.create_database = 'testSET.db'
+#sqlite1.create_table("test2")
+#sqlite1.create_table("test2")
+#sqlite1.insert_into_table("test2")
 #sqlite1.create_database("test1")
 #sqlite1.create_table("world")
