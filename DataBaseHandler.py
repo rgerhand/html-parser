5df1599a12c5f2902fbf1c1d5b1693aac8dc74a4
resti11 <rafalgerhand@gmail.com>
2023-01-30 14:01:58 +0100

    Add docstring and function annotations in DataBaseHandler.py

--- DataBaseHandler.py
+++ DataBaseHandler.py
@@ -1,14 +1,23 @@
+""" This file created and interface for databases"""
 from abc import abstractmethod, ABC
+
+"""
+    DatabaseMain class
+
+        Methods:
+            - login()               - responsible for login to database
+            - create_database()     - responsible for creating database
+"""
 
 
 class DatabaseMain(ABC):
     """ Database interface class"""
     @abstractmethod
-    def login(self, **kwargs):
+    def login(self, **kwargs: dict) -> True:
         """ This is abstract login method"""
         pass
 
     @abstractmethod
-    def create_database(self, database_name):
+    def create_database(self, database_name: str) -> True:
         """ This is abstract create database method"""
         pass
