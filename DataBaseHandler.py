47fa8d575147f2a60373e8c038e1a90ecc904b78
resti11 <rafalgerhand@gmail.com>
2023-02-09 22:47:57 +0100

    Refactor type hints in DataBaseHandler

--- DataBaseHandler.py
+++ DataBaseHandler.py
@@ -1,5 +1,6 @@
 """ This file creates the interface for databases"""
 from abc import abstractmethod, ABC
+from typing import Any
 
 """
     DatabaseMain class
@@ -18,31 +19,31 @@ class DatabaseMain(ABC):
     """ Database interface class"""
 
     @abstractmethod
-    def login(self, **kwargs) -> True:
+    def login(self, **kwargs) -> Any:
         """ Method to login with database"""
         pass
 
     @abstractmethod
-    def connection(self) -> True:
+    def connection(self) -> Any:
         """ Method to connect with database"""
         pass
 
     @abstractmethod
-    def cursor(self) -> True:
+    def cursor(self) -> Any:
         """ Method to create a cursor"""
         pass
 
     @abstractmethod
-    def create_database(self, database_name: str) -> True:
+    def create_database(self, database_name: str) -> None:
         """ Method to create database"""
         pass
 
     @abstractmethod
-    def create_table(self, table_name: str, **kwargs) -> True:
+    def create_table(self, table_name: str, **kwargs) -> None:
         """ Method to create table"""
         pass
 
     @abstractmethod
-    def insert_into_table(self, table_name: str, *args) -> True:
+    def insert_into_table(self, table_name: str, *args) -> None:
         """ Method to insert data into table"""
         pass
