0f4fea5e5653bee1799ef59e0eb837e71d159029
resti11 <rafalgerhand@gmail.com>
2023-02-15 23:02:31 +0100

    First tests

---
+++ tests/mock_tests.py
@@ -0,0 +1,34 @@
+from unittest.mock import MagicMock
+
+
+def my_function(a, b):
+    return a + b
+
+
+mock_function = MagicMock(return_value=3)
+result = mock_function(1, 2)
+try:
+    assert result == 2
+    print("Pass")
+except AssertionError:
+    print("Fail")
+
+
+class MyClass:
+    def my_method(self, a, b):
+        return a + b
+
+    def test(self):
+        mock_obj = MagicMock(spec=MyClass)
+        mock_obj.my_method.return_value = 3
+
+        result = mock_obj.my_method(1, 2)
+        try:
+            assert result == 3
+            print("Pass")
+        except AssertionError:
+            print("Fail")
+
+inst = MyClass()
+inst.test()
+
