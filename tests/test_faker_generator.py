77f3e0a5b621b77ce423d9b0e7cd0fdca3fc6730
resti11 <rafalgerhand@gmail.com>
2023-02-27 11:57:19 +0100

    Add faker_generator tests

---
+++ tests/test_faker_generator.py
@@ -0,0 +1,19 @@
+""" This file is responsible for faker_generator tests"""
+import pytest
+from faker_generator import generate_name, generate_address
+
+
+def test_generate_name():
+    """ Method to test generation of names """
+    names = generate_name(3)
+    assert len(names) == 3
+    assert isinstance(names, list)
+    assert isinstance(names[0], str)
+
+
+def test_generate_addresses():
+    """ Method to test generation of addresses """
+    addresses = generate_address(3)
+    assert len(addresses) == 3
+    assert isinstance(addresses, list)
+    assert isinstance(addresses[0], str)
