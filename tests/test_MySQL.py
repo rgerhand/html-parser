7990b1fbe58546d8e8b37e2d6542a804fe89073c
Rafal <rafal.gerhand@molex.com>
2023-03-14 15:19:10 +0100

    Example of MySQL tests

--- tests/test_MySQL.py
+++ tests/test_MySQL.py
@@ -1,15 +1,28 @@
 """ This file is responsible for mySQL tests"""
 import pytest
-from unittest.mock import MagicMock, patch
+from unittest.mock import Mock, patch
 from MySQLClass import MySQL
 
 
 # TODO
 class MySQLTest:
-    """ Method to test the connection """
-    def test_connection(self):
-        database_mock = MagicMock(spec=MySQL)
-        database_mock.user = "test"
-        database_mock.password = "test"
-        database_mock.host = "localhost"
-        database_mock.database_name = "testdb"
+    @patch('MySQLClass.MySQL.login')
+    def test_connection(self, mock_db):
+        """ Method to test the connection """
+        mock_db.return_value = 'MySQLConnection'
+        result = MySQL.login()
+        assert result == 'MySQLConnection'
+
+    @patch('MySQLClass.MySQL.cursor')
+    def test_cursor(self, mock_cursor):
+        """ Method to test the cursor """
+        mock_cursor.return_value = 'MySQLCursor'
+        result = MySQL.cursor()
+        assert result == 'MySQLCursor'
+
+
+inst = MySQLTest()
+inst.test_cursor()
+
+
+
